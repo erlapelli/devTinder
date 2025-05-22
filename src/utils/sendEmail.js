3433a54364dcf1a191338ef25600bb2791595c38
unknown <erlapellipoornachandhar@gmail.com>
2025-05-22 17:56:15 +0530

    adding env

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,67 @@
+const { SendEmailCommand } = require ("@aws-sdk/client-ses");
+const { sesClient } = require("./sesClient.js");
+
+
+const createSendEmailCommand = (toAddress, fromAddress,subject,body) => {
+  return new SendEmailCommand({
+    Destination: {
+      CcAddresses: [],
+      ToAddresses: [toAddress],
+    },
+    Message: {
+      
+      Body: {
+        /* required */
+        Html: {
+          Charset: "UTF-8",
+          Data: `<h1>${body}</h1>`,
+        },
+        Text: {
+          Charset: "UTF-8",
+          Data: "This is the text format email",
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: subject,
+      },
+    },
+    Source: fromAddress,
+    ReplyToAddresses: [
+      /* more items */
+    ],
+  });
+
+};
+
+
+const run = async (subject,body) => {
+  const sendEmailCommand = createSendEmailCommand(
+    "erlapellipoornachandhar30@gmail.com",
+    "pavanvasala799@gmail.com",
+    subject,
+    body
+
+  );
+
+  try {
+    console.log("📤 Sending email...");
+    
+    const result = await sesClient.send(sendEmailCommand);
+
+    
+    console.log("✅ Email sent successfully!");
+    console.log("SES Response:", result)
+    return result;
+  } catch (caught) {
+    if (caught instanceof Error && caught.name === "MessageRejected") {
+      
+      const messageRejectedError = caught;
+      return messageRejectedError;
+    }
+    throw caught;
+  }
+};
+
+// snippet-end:[ses.JavaScript.email.sendEmailV3]
+module.exports = { run };
